651e8fd9ce3d3563a3c06e011cfc81b88afcb9ac
Greg Aragon <garoc70@gmail.com>
2013-07-16 01:58:46 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
 var express = require('express');
 
+var fs = require('fs');
+
 var app = express.createServer(express.logger());
 
+var htmltext ="idk";
+
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+}); 
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(htmltext);
 });
 
 var port = process.env.PORT || 5000;
