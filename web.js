a72ddba7131b8b05006886e35b8d577afe372288
Greg Aragon <garoc70@gmail.com>
2013-07-16 02:03:40 +0000

    Updated web.js for index.html again

--- web.js
+++ web.js
@@ -4,8 +4,6 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-//var htmltext ="idk";
-
 var htmltext = fs.readFile('index.html', function (err, data) {
   if (err) throw err;
   console.log(data);
